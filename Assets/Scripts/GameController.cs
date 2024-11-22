ccaf051c277431a5fb5ddbc3fb20d2dd9bfb4a72
Lupeni Tuikalepa <38043285+LupeniTuikalepa@users.noreply.github.com>
2024-11-23 00:19:09 +0100

    Game Metrics

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,18 +4,31 @@ namespace FishyBusiness.Core
 {
     public static class GameController
     {
+        private static GameMetrics gameMetrics;
+
+        public static GameMetrics Metrics
+        {
+            get
+            {
+                if (gameMetrics == null)
+                    gameMetrics = Resources.Load<GameMetrics>("GameMetrics");
+
+                return gameMetrics;
+            }
+        }
+
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Load()
         {
             Application.wantsToQuit += UnLoad;
             Application.targetFrameRate = 60;
         }
-        
+
         private static bool UnLoad()
         {
             return true;
         }
-        
+
         public static void QuitGame()
         {
             Application.Quit();
