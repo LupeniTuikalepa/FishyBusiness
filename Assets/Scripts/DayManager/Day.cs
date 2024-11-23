83a5e7a58636a53df53a5344beaeaad3e312859c
Xictoss <147993401+Xictoss@users.noreply.github.com>
2024-11-23 19:58:04 +0100

    DayController SecondPart

--- Assets/Scripts/DayManager/Day.cs
+++ Assets/Scripts/DayManager/Day.cs
@@ -1,60 +1,92 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 namespace FishyBusiness.DayManager
 {
-    public class Day 
+    public class Day
     {
+        public event Action OnDayFinished;
+        public event Action<IDayChoice> OnNewChoice; 
+        
         private List<IDayChoice> _fishList;
         private IDayChoice currentFish;
-        public bool IsFinished => _fishList.Count == 0;
+        private int quota;
+        private int earnedMoney;
 
-        public Day(List<IDayChoice> choices)
+        public bool IsFinished => _fishList.Count == 0;
+        public bool IsQuotaReached => earnedMoney >= quota;
+
+        //constructor
+        public Day(List<IDayChoice> choices, int quota)
         {
+            earnedMoney = 0;
+            this.quota = quota;
             _fishList = choices;
+        }
+        //--//
+
+        public void Begin()
+        {
             ChooseRandomFish();
         }
-        
         private void ChooseRandomFish()
         {
             if (_fishList.Count <= 0)
             {
-                Debug.Log("jeu finito");
+                //Debug.Log("jeu finito");
                 return;
             }
             
-            Debug.Log("On choisit un poisson :");
+            //Debug.Log("On choisit un poisson :");
             int indexAleatoire = Random.Range(0, _fishList.Count);
             currentFish = _fishList[indexAleatoire];
             Debug.Log(currentFish);
+            OnNewChoice?.Invoke(currentFish);
         }
         
-        public bool AcceptChoice()
+        public bool AcceptChoice(out IDayChoice choice)
         {
+
+            choice = currentFish;
+            
             bool isRight = currentFish.IsTruth == true;
-                
+
+            if (isRight)
+            {
+                earnedMoney += currentFish.Money;
+            }    
             CompleteChoice();
             
-            Debug.Log("Le poisson est accepter, on lui file la cam");
+            //Debug.Log("Le poisson est accepter, on lui file la cam");
+            
             return isRight;
         }
 
-        public bool DeclineChoice()
+        public bool DeclineChoice(out IDayChoice choice)
         {
+            choice = currentFish;
+            
             bool isRight = currentFish.IsTruth == false;
 
             CompleteChoice();
             
-            Debug.Log("Le poisson est refuser, ça dégage");
+            //Debug.Log("Le poisson est refuser, ça dégage");
             return isRight;
         }
 
         private void CompleteChoice()
         {
-            
             _fishList.Remove(currentFish);
-            ChooseRandomFish();
+            if (IsFinished)
+            {
+                OnDayFinished?.Invoke();
+            }
+            else
+            {
+                ChooseRandomFish();
+            }
         }
         
     }
--- Assets/Scripts/DayManager/Interfaces/IDayChoice.cs
+++ Assets/Scripts/DayManager/Interfaces/IDayChoice.cs
@@ -3,5 +3,7 @@
     public interface IDayChoice
     {
         bool IsTruth { get; }
+        int Money { get; }
+        int Damage { get; }
     }
 }
