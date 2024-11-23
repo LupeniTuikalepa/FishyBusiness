c4d264b874665881992c454ea7f5776a3c9dfdfc
Xictoss <147993401+Xictoss@users.noreply.github.com>
2024-11-23 02:35:27 +0100

    DayController FirstPart

---
+++ Assets/Scripts/DayManager/Day.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace FishyBusiness.DayManager
+{
+    public class Day 
+    {
+        private List<IDayChoice> _fishList;
+        private IDayChoice currentFish;
+        public bool IsFinished => _fishList.Count == 0;
+
+        public Day(List<IDayChoice> choices)
+        {
+            _fishList = choices;
+            ChooseRandomFish();
+        }
+        
+        private void ChooseRandomFish()
+        {
+            if (_fishList.Count <= 0)
+            {
+                Debug.Log("jeu finito");
+                return;
+            }
+            
+            Debug.Log("On choisit un poisson :");
+            int indexAleatoire = Random.Range(0, _fishList.Count);
+            currentFish = _fishList[indexAleatoire];
+            Debug.Log(currentFish);
+        }
+        
+        public bool AcceptChoice()
+        {
+            bool isRight = currentFish.IsTruth == true;
+                
+            CompleteChoice();
+            
+            Debug.Log("Le poisson est accepter, on lui file la cam");
+            return isRight;
+        }
+
+        public bool DeclineChoice()
+        {
+            bool isRight = currentFish.IsTruth == false;
+
+            CompleteChoice();
+            
+            Debug.Log("Le poisson est refuser, ça dégage");
+            return isRight;
+        }
+
+        private void CompleteChoice()
+        {
+            
+            _fishList.Remove(currentFish);
+            ChooseRandomFish();
+        }
+        
+    }
+}
