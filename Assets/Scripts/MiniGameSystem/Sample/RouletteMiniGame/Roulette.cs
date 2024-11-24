827d8e937556bed915670ae6fd1727b65319394a
ZircliX <lolofy71@gmail.com>
2024-11-24 17:43:07 +0100

    MiniGames Update + Roulette

---
+++ Assets/Scripts/MiniGameSystem/Sample/RouletteMiniGame/Roulette.cs
@@ -0,0 +1,44 @@
+﻿using FishyBusiness.MiniGameSystem.Interfaces;
+using UnityEngine;
+
+namespace FishyBusiness.MiniGameSystem.Sample.RouletteMiniGame
+{
+    public class Roulette : MiniGame<RouletteContext>
+    {
+        private int rouletteResult = -1;
+        
+        public override void Begin(ref RouletteContext context)
+        {
+            context.status = GameStatus.Pending;
+            
+            rouletteResult = Random.Range(0, 2);
+        }
+
+        public override bool Refresh(ref RouletteContext context)
+        {
+            if (rouletteResult != -1)
+            {
+                if (rouletteResult == context.playerChoice)
+                {
+                    context.status = GameStatus.Success;
+                }
+                else
+                {
+                    context.status = GameStatus.Failure;
+                }
+            }
+
+            return context.status != GameStatus.Pending;
+        }
+
+        public override void End(ref RouletteContext context)
+        {
+            Debug.Log($"End Roulette : {context.status}");
+            if (context.status == GameStatus.Success)
+            {
+                context.Player.AddMoney(context.BetAmount * 2);
+            }
+            context.status = GameStatus.None;
+        }
+    }
+}
---
+++ Assets/Scripts/MiniGameSystem/Sample/RouletteMiniGame/RouletteContext.cs
@@ -0,0 +1,14 @@
+﻿using FishyBusiness.MiniGameSystem.Interfaces;
+
+namespace FishyBusiness.MiniGameSystem.Sample.RouletteMiniGame
+{
+    public struct RouletteContext : IMiniGameContext
+    {
+        public Player Player;
+        public int BetAmount;
+        public int playerChoice;
+        
+        public GameStatus Status => status;
+        public GameStatus status;
+    }
+}
