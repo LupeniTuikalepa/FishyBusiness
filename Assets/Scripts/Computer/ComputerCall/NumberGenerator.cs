8ecc1c3ebbe87c72e1b35e5c05bb1972dcb68ca4
Xictoss <147993401+Xictoss@users.noreply.github.com>
2024-11-24 18:28:48 +0100

    PhoneCallSytemeFinito

---
+++ Assets/Scripts/Computer/ComputerCall/NumberGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using Random = UnityEngine.Random;
+
+
+namespace FishyBusiness
+{
+    public class PhoneNumberGenerator : MonoBehaviour
+    {
+        [SerializeField] private NumPadManager _numPadManager;
+        [SerializeField] private TextMeshProUGUI _phoneGeneratedTxt;
+        [SerializeField] private TextMeshProUGUI _resultComparaisonTxt;
+        [SerializeField] private TextMeshProUGUI phoneCallText;
+        
+        public TMP_InputField inputField;
+        public UnityEvent OnRing;
+        public UnityEvent OnFailedRing;
+        
+        private string phoneNumberGenerated;
+        private bool hasPhone;
+        
+        private void Start()
+        {
+            SetPhoneNumber();
+            
+        }
+
+        public void SetPhoneNumber()
+        {
+            phoneNumberGenerated = PhoneNumberUtilities.GeneratePhoneNumber();
+            _phoneGeneratedTxt.text = phoneNumberGenerated;
+        }
+        
+        private void OnEnable()
+        {
+            _numPadManager.OnCallPressed += CheckNumber;
+        }
+
+
+        private void OnDisable()
+        {
+            _numPadManager.OnCallPressed -= CheckNumber;
+        }
+        
+        public void CheckNumber() 
+        {
+            hasPhone = Random.Range(0, 2) == 0;
+            
+            string userInputNumber = inputField.text;
+
+            if (userInputNumber == phoneNumberGenerated)
+            {
+                _resultComparaisonTxt.text = "Right Number";
+                _resultComparaisonTxt.color = Color.green;
+                if (hasPhone)
+                {
+                    phoneCallText.text = "Phone Ring";  
+                    phoneCallText.color = Color.cyan;
+                    OnRing.Invoke();
+                }
+                else
+                {
+                    phoneCallText.text = "No Phone Rings!";  
+                    phoneCallText.color = Color.red;
+                    OnFailedRing.Invoke();
+                }
+            }
+            else
+            {
+                _resultComparaisonTxt.text = "Wrong Number";
+                _resultComparaisonTxt.color = Color.red;
+            }
+        }
+    }
+}
