7456fc7c92c7c4b1bbcd80d2d0527cbb5b134277
Xictoss <147993401+Xictoss@users.noreply.github.com>
2024-11-25 17:31:32 +0100

    Sound WIP

---
+++ Assets/Scripts/Sounds/SoundsManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace FishyBusiness.Sounds
+{
+    public class SoundsManager : MonoBehaviour
+    {
+        [SerializeField] private AudioSource AudioSourceClicked;
+        
+        
+        [SerializeField] private AudioSource AudioSource;
+
+
+        
+        
+        private void OnEnable()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnDisable()
+        {
+            throw new NotImplementedException();
+        }
+        
+        
+        
+    }
+}
