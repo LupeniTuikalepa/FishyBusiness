ac568982a3aefb1e349188a431b5a60022da6008
Xictoss <147993401+Xictoss@users.noreply.github.com>
2024-11-25 21:13:27 +0100

    MainMenuSettingsFinito

--- Assets/Scripts/MenuSystem/MenuData/SettingsMenu.cs
+++ Assets/Scripts/MenuSystem/MenuData/SettingsMenu.cs
@@ -1,7 +1,39 @@
-﻿namespace FishyBusiness.MenuSystem.MenuData
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace FishyBusiness.MenuSystem.MenuData
 {
     public class SettingsMenu : BaseMenu
     {
+        public Slider musicSlider;
+        public Slider sfxSlider;
+        public AudioSource musicSource;
+        public AudioSource sfxSource;
+
+        void Start()
+        {
+            
+            if (musicSource != null)
+                musicSlider.value = musicSource.volume;
+
+            if (sfxSource != null)
+                sfxSlider.value = sfxSource.volume;
+        }
         
+        public void SetMusicVolume(float volume)
+        {
+            if (musicSource != null)
+            {
+                musicSource.volume = volume;
+            }
+        }
+        
+        public void SetSFXVolume(float volume)
+        {
+            if (sfxSource != null)
+            {
+                sfxSource.volume = volume;
+            }
+        }
     }
 }
